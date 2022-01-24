46ab60bc5133eaa648807519cab794297fe72758
lEhUyh0Ang <98325915+lEhUyh0Ang@users.noreply.github.com>
2022-01-24 20:35:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    titleWeb: "Tmặng bmạn",
+    introTitle: 'Anh bạn à!!!',
+    introDesc: `Anh có thể bấm vào nút phía dưới được không.`,
+    btnIntro: '^_^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên???',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Thík nhắm',
+    btnNo: 'Hông bé ơi ;-;',
+    question: 'Tại sao bạn lại dễ thương đến vậy, yêu mình hay gì <3',
+    btnReply: 'Chấp nhận đy mờ :3',
+    reply: 'Đơn giản chỉ là YÊU thôi <3',
+    mess: 'Mình biết mà. Yêu bạn nhiều',
+    messDesc: 'Làm ngừi iu mìk nhoa.',
+    btnAccept: 'Oki lun =))',
+    messLink: 'https://www.messenger.com/t/100053847940983'
 }
